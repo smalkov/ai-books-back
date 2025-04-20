cf5e9852cb2d7ab75af08f8802e810169b5429d4
Малков Сергей Юрьевич <pewpewinmyhead@gmail.com>
2025-04-20 19:55:30 +0300

    fx

--- api.py
+++ api.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, HTTPException, Query
+from fastapi import FastAPI, Query
 from fastapi.responses import JSONResponse
 from fastapi.middleware.cors import CORSMiddleware
 
